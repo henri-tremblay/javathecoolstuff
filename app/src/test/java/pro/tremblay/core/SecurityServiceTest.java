17e8d933261193748584d4a7576dab1fae7bc6ca
Henri Tremblay <henri.tremblay@gmail.com>
2022-10-16 22:08:55 -0400

    Test for SecurityService

---
+++ app/src/test/java/pro/tremblay/core/SecurityServiceTest.java
@@ -0,0 +1,26 @@
+package pro.tremblay.core;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SecurityServiceTest {
+
+    private final SecurityService securityService = new SecurityService(Paths.get("../listing_short.csv"));
+
+    @BeforeEach
+    @AfterEach
+    void after() {
+        securityService.clear();
+    }
+
+    @Test
+    void allSecurities() {
+        assertThat(securityService.allSecurities()).containsExactlyInAnyOrderElementsOf(SecuritiesForTest.SECURITIES);
+    }
+
+}
