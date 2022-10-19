d92cc2eaf93bcc189e3dc8012fc128f3e8145c44
Henri Tremblay <henri.tremblay@gmail.com>
2022-10-19 08:02:53 -0700

    Add bootstrap

--- app/src/main/java/pro/tremblay/core/Main.java
+++ app/src/main/java/pro/tremblay/core/Main.java
@@ -31,9 +31,12 @@ public class Main {
         "<head>\n" +
         "    <meta charset=\"UTF-8\">\n" +
         "    <title>Return on investment</title>\n" +
+        "    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">" +
         "</head>\n" +
         "<body>\n" +
-        "Your current return on investment as of ${now} is: ${roi}\n" +
+        "    <div class=\"alert alert-primary\" role=\"alert\">\n" +
+        "        Your current return on investment as of ${now} is: ${roi}\n" +
+        "    </div>" +
         "</body>\n" +
         "</html>";
 
